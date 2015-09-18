9b74d787bffb5adcb014316c92561de206e36353
Danny Berger <dpb587@gmail.com>
2015-09-18 13:43:57 -0600

    Allow exiting after number of tasks or uptime

--- src/TLE/ElfBot/Console/Command/WorkCommand.php
+++ src/TLE/ElfBot/Console/Command/WorkCommand.php
@@ -3,6 +3,7 @@
 namespace TLE\ElfBot\Console\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Monolog\Logger;
 
@@ -13,6 +14,8 @@ class WorkCommand extends AbstractCommand
         parent::configure()
             ->setName('work')
             ->setDescription('Work from the queue')
+            ->addOption('max-tasks', null, InputOption::VALUE_REQUIRED, 'Exit cleanly after this many tasks')
+            ->addOption('max-lifetime', null, InputOption::VALUE_REQUIRED, 'Exit cleanly after this many seconds of running')
         ;
     }
 
@@ -22,6 +25,16 @@ class WorkCommand extends AbstractCommand
 
         $logger = $container['logger'];
         $wantsExit = false;
+
+        $tasksLimit = $input->getOption('max-tasks');
+        $tasksCount = 0;
+
+        $lifetimeStart = time();
+        $lifetimeLimit = $input->getOption('max-lifetime');
+
+        if (null !== $lifetimeLimit) {
+            $lifetimeLimit += $lifetimeStart;
+        }
 
         $signals = function () use (&$wantsExit, $logger) {
             $logger->info('received signal to exit');
@@ -34,24 +47,20 @@ class WorkCommand extends AbstractCommand
         pcntl_signal(SIGINT, $signals);
         pcntl_signal(SIGTERM, $signals);
 
-        $taskCallback = function ($id, $task, array $options) use ($container) {
+        $taskCallback = function ($id, $task, array $options) use ($container, &$tasksCount) {
+            $tasksCount += 1;
+
             $logger = new Logger(sprintf('%s/%s', $container['runtime.name'] . ($container['logger.channel'] ? ('/' . $container['logger.channel']) : ''), $id));
             $logger->pushHandler($container['logger.handler']);
 
+            $logger->debug($task . ' started');
+
+            $mt = microtime(true);
+
             try {
-                $logger->debug($task . ' started');
-
-                $mt = microtime(true);
-
                 $container['task_factory']->execute($logger, $task, $options);
-
-                $duration = (microtime(true) - $mt) * 1000;
-
-                $logger->info($task . ' completed (' . $duration . 'ms)');
-
-                return true;
             } catch (\Exception $e) {
-                $duration = (microtime(true) - $mt) * 1000;
+                $duration = ceil((microtime(true) - $mt) * 1000);
 
                 $logger->info($task . ' failed (' . $duration . 'ms)');
 
@@ -70,10 +79,26 @@ class WorkCommand extends AbstractCommand
 
                 return false;
             }
+
+            $duration = ceil((microtime(true) - $mt) * 1000);
+
+            $logger->info($task . ' completed (' . $duration . 'ms)');
+
+            return true;
         };
 
         while (!$wantsExit) {
             $container['worker']->work($taskCallback);
+
+            if ((null !== $tasksLimit) && ($tasksCount >= $tasksLimit)) {
+                $logger->warn(sprintf('stopping (%s tasks were run)', $tasksCount));
+
+                $wantsExit = true;
+            } elseif ((null !== $lifetimeLimit) && (time() >= $lifetimeLimit)) {
+                $logger->warn(sprintf('stopping (lived %s seconds)', (time() - $lifetimeStart)));
+
+                $wantsExit = true;
+            }
         }
 
         $logger->info('done');
