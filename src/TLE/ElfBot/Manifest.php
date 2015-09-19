c4d63a9c80ea7dec5e2d9ee2cd2a55e29a5ecbe5
Danny Berger <dpb587@gmail.com>
2015-09-19 11:30:03 -0600

    Fix manifest debug line

--- src/TLE/ElfBot/Manifest.php
+++ src/TLE/ElfBot/Manifest.php
@@ -4,6 +4,6 @@ namespace TLE\ElfBot;
 
 class Manifest {
     const NAME = 'elfbot';
-    const VERSION = 'v2.0.0';//@git-version@';
+    const VERSION = '@git-version@';
     const COMMIT = '@git-commit@';
 }
